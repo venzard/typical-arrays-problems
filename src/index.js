e31bfcf4763510740599e4ff4f211ae16639e840
venzard <vasilij-ch@ya.ru>
2021-08-19 18:54:39 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0
+  }else{
+    array.sort( (a, b) => a - b );
+    return array[0];
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array.length === 0){
+    return 0
+  }else{
+    array.sort( (a, b) => a - b );
+    return array[(array.length)-1];
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let i=0;
+  let sum=0;
+  let count=0;
+  let res=0;
+  if (array === undefined || array.length === 0){
+    return 0
+  }else{
+    for (i=0;i<array.length;i++){
+      sum+=array[i];
+      count++;
+    }
+    res = sum / count;
+    return res;
+  }
 }
